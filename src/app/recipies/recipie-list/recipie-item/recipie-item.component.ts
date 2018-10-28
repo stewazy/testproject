73ffe785b49766a35afaeb3398ec97f3bd956b33
stewazy <stewazy@gmail.com>
2018-10-28 09:36:41 +0100

    Signed-off-by: stewazy <stewazy@gmail.com>

--- src/app/recipies/recipie-list/recipie-item/recipie-item.component.ts
+++ src/app/recipies/recipie-list/recipie-item/recipie-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipie } from './../../recipie.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipie-item',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-item.component.css']
 })
 export class RecipieItemComponent implements OnInit {
-
+  @Input() recipie: Recipie;
   constructor() { }
 
   ngOnInit() {
