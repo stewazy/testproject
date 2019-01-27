be9123eb7404d81b80ee1b4bae9f4f3498bdfbaf
stewazy <stewazy@gmail.com>
2019-01-27 10:19:24 +0100

    Signed-off-by: stewazy <stewazy@gmail.com>

--- src/app/recipies/recipie-edit/recipie-edit.component.ts
+++ src/app/recipies/recipie-edit/recipie-edit.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipieEditComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  editMode = false;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null;
+        console.log(this.editMode);
+      }
+    );
   }
 
 }
