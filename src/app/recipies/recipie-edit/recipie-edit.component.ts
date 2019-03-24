4183f660624c301eb2ab69c7a2425b4d57bbbd1e
stewazy <stewazy@gmail.com>
2019-03-24 08:52:59 +0100

    Signed-off-by: stewazy <stewazy@gmail.com>

--- src/app/recipies/recipie-edit/recipie-edit.component.ts
+++ src/app/recipies/recipie-edit/recipie-edit.component.ts
@@ -1,5 +1,7 @@
+import { RecipieService } from './../recipie.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-recipie-edit',
@@ -10,14 +12,39 @@ export class RecipieEditComponent implements OnInit {
 
   id: number;
   editMode = false;
-  constructor(private route: ActivatedRoute) { }
+  recipieForm: FormGroup;
+
+  constructor(private route: ActivatedRoute,
+    private recipieService: RecipieService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
         console.log(this.editMode);
+      }
+    );
+  }
+
+  private initForm() {
+
+    let recipieName = '';
+    let recipieImagePath = '';
+    let recipieDescription = ''l
+
+    if(this.editMode) {
+      const recipie = this.recipieService.getRecipie(this.id);
+      recipieName = recipie.name;
+      recipieImagePath = recipie.imagePath;
+      recipieDescription = recipie.description;
+    }
+    this.recipieForm = new FormGroup(
+      {
+        'name': new FormControl(recipieName),
+        'imagePath': new FormControl(recipieImagePath),
+        'description': new FormControl(recipieDescription)
       }
     );
   }
